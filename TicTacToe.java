e53813005e5bd55ed1488c0261c27ce2165771b0
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-21 08:33:55 +0530

    Add: Empty TicTacToe Game Board

---
+++ TicTacToe.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.workshop;
+
+public class TicTacToe {
+	public static void game() {
+		char[] board= {'0','0','0','0','0','0','0','0','0','0'};
+		for (int i=1;i<10;i++) {
+			if(i==4 || i==7 ) {
+				System.out.println("");
+			}
+			System.out.print(" "+board[i]);
+		}
+	}
+	
+  public static void main(String[] args) {
+	  game();
+    
+  }
+}
